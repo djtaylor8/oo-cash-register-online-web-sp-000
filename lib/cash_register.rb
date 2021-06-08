dec3ff659c32ea81dfd36e286613a5a4e8e3c720
DJ Taylor <dtaylor88@gmail.com>
2021-06-08 03:52:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount
+  @@items = []
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount 
+  end 
+  
+  def total
+    @total 
+  end 
+  
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity 
+    @@items << title
+  end 
+  
+  def apply_discount
+    if @discount > 0 
+      @total -= @discount 
+      puts "After the discount, the total comes to #{@total}."
+    elsif @discount == nil  
+      puts "There is no discount to apply."
+    end 
+  end 
+  
+  def items
+    @@items
+  end 
+  
+  def void_last_transaction
+  end 
+  
+  
+end 
