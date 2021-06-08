ec43b6735509063e2960cb6b4d4795932a98ba20
DJ Taylor <dtaylor88@gmail.com>
2021-06-08 03:59:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,7 +23,6 @@ class CashRegister
     if @discount 
       @total -= @discount * 10 
       puts "After the discount, the total comes to #{@total}."
-      binding.pry 
     else  
       puts "There is no discount to apply."
     end 
